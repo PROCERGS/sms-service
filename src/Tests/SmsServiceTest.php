f52fb516d6cd9d3dad29d8ade571fa0428b53ec6
Guilherme Donato <guilhermednt@users.noreply.github.com>
2017-04-03 15:22:19 -0300

    added missing tests

--- src/Tests/SmsServiceTest.php
+++ src/Tests/SmsServiceTest.php
@@ -71,11 +71,29 @@ class SmsServiceTest extends \PHPUnit_Framework_TestCase
         $this->sendSms($smsService, $this->getValidPhoneNumber(), "this should fail");
     }
 
-    public function testForceReceiveAll()
+    public function testForceReceive()
     {
+        $messages = [
+            [
+                "id" => 123,
+                "de" => "5551999999999",
+                "para" => "666",
+                "mensagem" => "Message 1",
+                "dataHoraRecebimento" => "2016-07-07T15:57:35.000-03:00",
+            ],
+            [
+                "id" => 321,
+                "de" => "5554999999999",
+                "para" => "666",
+                "mensagem" => "Message 2",
+                "dataHoraRecebimento" => "2016-11-23T14:23:02.000-03:00",
+            ],
+        ];
+
+        $lastId = 123;
         $response = $this->getMock('Symfony\Component\HttpFoundation\Response');
         $response->expects($this->once())->method('isOk')->willReturn(true);
-        $response->expects($this->once())->method('getContent')->willReturn(json_encode([['x' => 'oi']]));
+        $response->expects($this->once())->method('getContent')->willReturn(json_encode($messages));
 
         $restClient = $this->getRestClient();
         $restClient->expects($this->once())->method('get')->willReturn($response);
@@ -85,9 +103,29 @@ class SmsServiceTest extends \PHPUnit_Framework_TestCase
         /** @var SmsService $smsService */
         $smsService = $this->getSmsService($restClient);
 
-        $allSms = $smsService->forceReceive($tag);
-        $this->assertNotEmpty($allSms);
-        //$this->fail("TPD service unavailable. couldn't get sample responde");
+        $received = $smsService->forceReceive($tag, $lastId);
+        $this->assertNotEmpty($received);
+        $this->assertCount(count($messages), $received);
+    }
+
+    public function testForceReceiveError()
+    {
+        $this->setExpectedException('PROCERGS\Sms\Exception\SmsServiceException');
+
+        $lastId = 123;
+        $response = $this->getMock('Symfony\Component\HttpFoundation\Response');
+        $response->expects($this->once())->method('isOk')->willReturn(true);
+        $response->expects($this->atLeastOnce())->method('getContent')->willReturn('error');
+
+        $restClient = $this->getRestClient();
+        $restClient->expects($this->once())->method('get')->willReturn($response);
+
+        $tag = 'tag';
+
+        /** @var SmsService $smsService */
+        $smsService = $this->getSmsService($restClient);
+
+        $smsService->forceReceive($tag, $lastId);
     }
 
     public function testStatus()
@@ -116,6 +154,36 @@ class SmsServiceTest extends \PHPUnit_Framework_TestCase
 
         $first = reset($status);
         $this->assertEquals($transactionId, $first->numero);
+    }
+
+    public function testStatusError()
+    {
+        $errorResponse = json_encode(
+            [
+                [
+                    "id" => null,
+                    "message" => "Protocolo 'a' inválido, deve conter apenas números.",
+                    "field" => null,
+                    "detail" => null,
+                ],
+            ]
+        );
+        $error = json_decode($errorResponse);
+        $expectedMessage = reset($error)->message;
+
+        $this->setExpectedException('PROCERGS\Sms\Exception\SmsServiceException', $expectedMessage);
+
+        $transactionId = 'error';
+        $response = $this->getMock('Symfony\Component\HttpFoundation\Response');
+        $response->expects($this->once())->method('isOk')->willReturn(false);
+        $response->expects($this->once())->method('getContent')->willReturn($errorResponse);
+
+        $restClient = $this->getRestClient();
+        $restClient->expects($this->once())->method('get')->willReturn($response);
+
+        /** @var SmsService $smsService */
+        $smsService = $this->getSmsService($restClient);
+        $smsService->getStatus($transactionId);
     }
 
     private function getRestClient($response = null)
